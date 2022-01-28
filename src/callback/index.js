a691f0b1aa1dc27add7df0179ec96356c8ae266b
Griger <grigerratia@gmail.com>
2022-01-27 20:51:53 -0400

    Estos son los archivos de la clase de definicion de estructura callback

---
+++ src/callback/index.js
@@ -0,0 +1,29 @@
+function sum(n1, n2) {
+    return n1 + n2
+}
+
+function calc(n1, n2, callback) {
+    return callback(n1, n2)
+}
+
+console.log(calc(5, 5, sum))
+
+
+
+
+
+function date(callback) {
+    
+    setTimeout(function () {
+        let date = new Date
+        callback(date)
+    }, 0)
+
+    console.log(new Date + " sin tiempo")
+}
+
+function printDate(dateNow) {
+    console.log(dateNow + " con tiempo")
+}
+
+date(printDate)
